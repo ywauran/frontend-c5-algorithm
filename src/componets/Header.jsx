5b86b6c253b86ce79006ef35d4947ac0bd234703
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-05-18 20:59:48 +0800

    change header

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -3,9 +3,14 @@ import React from "react";
 const Header = ({ title }) => {
   return (
     <>
-      <div className="bg-white p-3 font-semibold border-b-[1px] border-gray-200 text-xl">
-        {title}
-      </div>
+      <header className="bg-white p-3 font-semibold border-b-[1px] border-gray-200 text-xl">
+        <div>
+          <h1 className="text-center">
+            Aplikasi Prediksi Tingkat Kesejahteraan
+          </h1>
+          <h3 className="mt-4 text-lg">{title}</h3>
+        </div>
+      </header>
     </>
   );
 };
